ac7d1ae9fc937091d2f5c6e97a87e895b369536f
Samin Yead <Saminyead@gmail.com>
2023-03-05 23:30:02 +0600

    gpu recommendation for 1 higher price tier added

--- app_test.py
+++ app_test.py
@@ -59,7 +59,7 @@ def get_best_card_df(budget:int = budget_input, which_query:str = "current"):
     which_query_dict = {
         "current" : f"SELECT * FROM lowest_prices_tiered WHERE gpu_price <= {budget} ORDER BY {tier_score_col} DESC LIMIT 1",
         "lower" : f"SELECT * FROM lowest_prices_tiered WHERE gpu_price < {budget} ORDER BY {tier_score_col} DESC LIMIT 1",
-        "higher" : f"SELECT * FROM lowest_prices_tiered WHERE gpu_price > {budget} ORDER BY {tier_score_col} ASC LIMIT 1",
+        "higher" : f"SELECT * FROM lowest_prices_tiered WHERE gpu_price > {budget} ORDER BY gpu_price ASC LIMIT 1",
     }
     query_price = which_query_dict[which_query]
     query_best_card_df = pd.read_sql(sql = query_price, con = conn)
@@ -88,29 +88,57 @@ def upon_budget_input():
         too_low_gtx_1050_ti()
     else:
         # writing recommended GPU
-        st.write(f"Buy {get_best_card_df().gpu_unit_name[0]}")
+        recommended_gpu_unit_name = get_best_card_df().gpu_unit_name[0]
+        st.write(f"Buy {recommended_gpu_unit_name}")
         recommended_gpu_df_price = get_best_card_df().gpu_price[0]
         recommended_gpu_df = get_best_cards_all(recommended_gpu_df_price)
         recommended_gpu_tier_score = recommended_gpu_df.iloc[0][tier_score_col]
         recommended_gpu_price_per_tier = recommended_gpu_df.iloc[0][price_per_tier_score]
         st.write(recommended_gpu_df)
 
+        # finding out GPU 1 price tier lower
         df_1_lower = get_best_card_df(budget=recommended_gpu_df_price,which_query="lower")
-        price_per_tier_1_lower = df_1_lower.iloc[0][price_per_tier_score]
-        tier_score_1_lower = df_1_lower.iloc[0][tier_score_col]
-        tier_diff_1_lower = abs(tier_score_1_lower - recommended_gpu_tier_score)
-        tier_diff_pct_1_lower = tier_diff_1_lower / recommended_gpu_tier_score * 100
+        # for the GTX 1050 Ti, df_1_lower becomes an empty dataframe
+        if len(df_1_lower) != 0:
+            df_1_lower_price = df_1_lower.gpu_price[0]
+            df_1_lower_gpu_unit = df_1_lower.gpu_unit_name[0]
+            price_per_tier_1_lower = df_1_lower.iloc[0][price_per_tier_score]
+            tier_score_1_lower = df_1_lower.iloc[0][tier_score_col]
+            tier_diff_1_lower = abs(tier_score_1_lower - recommended_gpu_tier_score)
+            tier_diff_pct_1_lower = tier_diff_1_lower / recommended_gpu_tier_score * 100
+            price_diff_1_lower = abs(recommended_gpu_df_price - df_1_lower_price)
+            price_diff_1_lower_pct = price_diff_1_lower / recommended_gpu_df_price * 100
+
+        # finding out GPU 1 price tier higher
+        df_1_higher = get_best_card_df(which_query="higher")
+        # expecting something similar to the df_1_lower happening
+        if len(df_1_higher) != 0:
+            df_1_higher_price = df_1_higher.gpu_price[0]
+            df_1_higher_gpu_unit = df_1_higher.gpu_unit_name[0]
+            price_per_tier_1_higher = df_1_higher.iloc[0][price_per_tier_score]
+            tier_score_1_higher = df_1_higher.iloc[0][tier_score_col]
+            tier_diff_1_higher = abs(tier_score_1_higher - recommended_gpu_tier_score)
+            tier_diff_pct_1_higher = tier_diff_1_higher / recommended_gpu_tier_score * 100
+            price_diff_1_higher = abs(recommended_gpu_df_price - df_1_higher_price)
+            price_diff_1_higher_pct = price_diff_1_higher / recommended_gpu_df_price * 100
         
         # for recommending better value GPU 1 price tier below
-        if tier_diff_pct_1_lower < 15 and price_per_tier_1_lower < recommended_gpu_price_per_tier:
-            df_1_lower_price = df_1_lower.gpu_price[0]
-            price_diff_1_lower = recommended_gpu_df_price - df_1_lower_price
-            price_diff_1_lower_pct = price_diff_1_lower / recommended_gpu_df_price * 100
-            df_1_lower_all = get_best_cards_all(price = df_1_lower_price)
-            st.write(
-                f"Save BDT. {price_diff_1_lower:,} by getting the {df_1_lower.gpu_unit_name[0]}. Provides within {round(tier_diff_pct_1_lower,2)}% of the value of {recommended_gpu_df.gpu_unit_name[0]} while costing {round(price_diff_1_lower_pct,2)}% lower"
-            )
-            st.write(df_1_lower_all)
+        if len(df_1_lower) != 0:
+            if price_per_tier_1_lower < recommended_gpu_price_per_tier and tier_diff_pct_1_lower < 15:
+                df_1_lower_all = get_best_cards_all(price = df_1_lower_price)
+                st.write(
+                    f"Save BDT. {price_diff_1_lower:,} by getting the {df_1_lower_gpu_unit}. Provides within {round(tier_diff_pct_1_lower,2)}% of the value of {recommended_gpu_unit_name} while costing {round(price_diff_1_lower_pct,2)}% lower"
+                )
+                st.write(df_1_lower_all)
+        
+        # for recommending better value GPU 1 price tier higher
+        if len(df_1_higher) != 0:
+            if price_per_tier_1_higher < recommended_gpu_price_per_tier and price_diff_1_higher_pct < 15:
+                df_1_higher_all = get_best_cards_all(price = df_1_higher_price)
+                st.write(
+                    f"Get the {df_1_higher_gpu_unit} for BDT. {price_diff_1_higher:,} more. Provides {round(tier_diff_pct_1_higher,2)}% higher value compared to the {recommended_gpu_unit_name} for just {round(price_diff_1_higher_pct,2)}% higher."
+                )
+                st.write(df_1_higher_all)
 
 
 
