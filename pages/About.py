6b26c77cc0643f6df092458a1d8c5aee88fad252
Samin Yead <Saminyead@gmail.com>
2023-04-05 20:38:35 +0600

    spellings updates

--- pages/About.py
+++ pages/About.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.write(
     """
     # About This App   
-    Are you a Bangladeshi gamer looking to buy a GPU? Do you wish you had a crystal ball to which you could just tell your budget, and
+    Are you a Bangladeshi gamer looking to buy a graphics card? Do you wish you had a crystal ball to which you could just tell your budget, and
     it would just tell you what the best GPU in the market is right now? Then this app is the crystal ball for you! Right after the pandemic chip
     shortage ended, the dollar crisis started making the GPU prices in Bangladesh all over the place. Sometimes, newer generation products can be 
     had for less than older generation products (just compare the price of Radeon RX 6950 XT vs the RX 7900 XTX). Hopefully, this app will help 
@@ -16,14 +16,14 @@ st.write(
     """ 
     Data is collected across the websites of 9 of the largest computer hardware retailers of Bangladesh, and stored on to a database. When you 
     enter your budget, the app checks for the graphics card with the highest performance score, and present it to you. To choose which performance
-    score, select the options "Yes" or "No" under the selection option "According to you, is Ray Tracing worth paying more for?", and then choose
-    whether you want to consider both positive and negative traits of GPU, or if you are interested in the raw performance only. You can find more
-    details about performance scores [here](https://github.com/Saminyead/gpu_for_bd_gamers/blob/master/docs/tier_score_simplified.md). Also, if you
-    want to simply want to know all the information about a particular GPU in the market, then check the "All GPU Information" page.
+    score, select the options "Yes" or "No" under the selection option "According to you, is Ray Tracing worth paying more for?". If you choose "No",
+    then choose whether you want to consider both positive and negative traits of GPU, or if you are interested in the raw performance only. You can 
+    find more details about performance scores [here](https://github.com/Saminyead/gpu_for_bd_gamers/blob/master/docs/tier_score_simplified.md). 
+    Also, if you simply want to know all the information about a particular GPU in the market, then check the "All GPU Information" page.
     """
 )
 
 st.write("""
-This is my first web application, and as a result may be a bit lacking in user experience. I hope to keep improving it, and add more features
-for the benefit of Bangladeshi gamers. I hope this will help you get the best price-to-performance for your money.
+This is my first web application, and as a result may be a bit lacking in user experience. I plan to keep improving it, and adding more features. 
+Hopefully, this will help you get the best price-to-performance for your money.
 """)
